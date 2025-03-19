06e22b2c87c1e7972f32a2a873bd3db1d76991cb
danilo <danilocencigg@gmail.com>
2025-03-19 15:32:56 -0300

    Add project files.

---
+++ CorridaDadosCs/Game.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CorridaDadosCs
+{
+    class Game
+    {
+        static int RandGenerator()
+        {
+            Random randGen = new Random();
+            int randNumber = randGen.Next(1, 7);
+
+            return randNumber;
+        }
+
+
+        public static void Play(int turn,int player1,int player2,int lenght)
+        {
+            if (player1 >= lenght)
+            {
+                Console.WriteLine("Vitória do Jogador 1!");
+                Program.EndScreen();
+            } else if (player2 >= lenght)
+            {
+                Console.WriteLine("Vitória do Jogador 2!");
+                Program.EndScreen();
+            }
+
+            Console.Clear();
+            Console.WriteLine("\n\n*CORRIDA*\n\n");
+            Console.WriteLine($"Jogador 1 (Você): {player1} de {lenght} casas");
+            Console.WriteLine($"Jogador 2: {player2} de {lenght} casas\n\n");
+            Console.WriteLine("Pressione Enter para Continuar.");
+            Console.ReadLine();
+
+            if (turn == 1)
+            {
+                Console.WriteLine("Vez do Jogador 1\n");
+                Roll1(player1, player2, lenght);
+            } else if (turn == 2)
+            {
+                Console.WriteLine("Vez do Jogador 2\n");
+                Roll2(player1, player2, lenght);
+            }
+        }
+
+
+
+        static void Roll1(int player1, int player2, int lenght)
+        {
+            int turn = 1;
+            int generatedNum = RandGenerator();
+            player1 = player1 + generatedNum;
+            string casas;
+            if (generatedNum == 1)
+            {
+                casas = "casa";
+            }
+            else
+            {
+                casas = "casas";
+            }
+            if (generatedNum == 6)
+            {
+                Console.WriteLine("O dado caiu no 6! um turno extra será concedido ao Jogador 1\nPressione Enter para continuar\n");
+                Console.ReadLine();
+            } else
+            {
+                turn = 2;
+            }
+            Console.WriteLine($"O Jogador 1 andará {generatedNum} {casas} \nPressione Enter para continuar\n\n");
+            Console.ReadLine();
+            
+            Play(turn,player1,player2,lenght);
+        }
+
+        static void Roll2(int player1, int player2, int lenght)
+        {
+            int turn = 2;
+            int generatedNum = RandGenerator();
+            player2 = player2 + generatedNum;
+            string casas;
+            if (generatedNum == 1)
+            {
+                casas = "casa";
+            } else
+            {
+                casas = "casas";
+            }
+
+            if (generatedNum == 6)
+            {
+                Console.WriteLine("O dado caiu no 6! um turno extra será concedido ao Jogador 2\nPressione Enter para continuar\n");
+                Console.ReadLine();
+            }
+            else
+            {
+                turn = 1;
+            }
+            Console.WriteLine($"O Jogador 2 andará {generatedNum} {casas}\nPressione Enter para continuar\n");
+            Console.ReadLine();
+
+            Play(turn, player1, player2, lenght);
+        }
+
+
+
+
+    }
+    
+    
+}
---
+++ CorridaDadosCs/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace CorridaDadosCs
+{
+    internal class Program
+    {
+        static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("*Corrida de Dados\n\n");
+            Console.WriteLine("1.Jogar\n2.Sair");
+            string menuRequest = Console.ReadLine();
+            if (menuRequest == "1")
+            {
+                SecondaryMenu();
+            } else if (menuRequest == "2")
+            {
+                Console.WriteLine("Finalizando o Programa...");
+                Environment.Exit(0);
+            }
+        }
+
+
+        static void SecondaryMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha a Duração da Corrida:\n\n1.Curta (20 Casas)\n2.Média (35 Casas)\n3.Longa (50 casas)");
+            Console.WriteLine("------------------");
+            Console.WriteLine("4.Voltar ao Menu Principal\n");
+            string menuRequest = Console.ReadLine();
+            if (menuRequest == "1")
+            {
+                Game.Play(1, 1, 1, 20);
+            } else if (menuRequest == "2")
+            {
+                Game.Play(1, 1, 1, 35);
+            } else if (menuRequest == "3")
+            {
+                Game.Play(1, 1, 1, 50);
+            } else if (menuRequest == "4")
+            {
+                MainMenu();
+            }
+        }
+
+        public static void EndScreen()
+        {
+            Console.WriteLine("\nJogo Finalizado, Deseja Jogar Novamente? (y/n)\n");
+            string answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                SecondaryMenu();
+            } else if (answer == "n")
+            {
+                Console.WriteLine("Finalizando o Programa...");
+                Environment.Exit(0);
+            } else
+            {
+                Console.WriteLine("Erro, Digite uma Opção Válida.\n");
+                EndScreen();
+            }
+
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+    }
+}
